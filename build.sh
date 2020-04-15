f7c85707659c8c7b2d0ca2d6823a1f4565d788d8
Catalin Manolescu <cc.manolescu@gmail.com>
2020-04-15 12:47:29 +0300

    update terraform version

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. .env
+
+# terraform releases https://releases.hashicorp.com/terraform/
+
+
+DOCKER_IMAGE=tinslice/terraform
+
+DOCKER_IMAGE_TAG=$TERRAFORM_VERSION
+echo "== build base docker image $DOCKER_IMAGE:$DOCKER_IMAGE_TAG =="
+docker build --pull -f $RUN_DIR/base/Dockerfile \
+       --build-arg ALPINE_VERSION=$ALPINE_VERSION --build-arg TERRAFORM_VERSION=$TERRAFORM_VERSION \
+       -t $DOCKER_IMAGE:$DOCKER_IMAGE_TAG $RUN_DIR/base
+docker push $DOCKER_IMAGE:$DOCKER_IMAGE_TAG
+
+DOCKER_IMAGE_TAG=$TERRAFORM_VERSION-awscli
+echo "== build awscli docker image $DOCKER_IMAGE:$DOCKER_IMAGE_TAG =="
+docker build -f $RUN_DIR/aws/Dockerfile \
+       --build-arg TERRAFORM_VERSION=$TERRAFORM_VERSION \
+       -t $DOCKER_IMAGE:$DOCKER_IMAGE_TAG $RUN_DIR/aws
+docker push $DOCKER_IMAGE:$DOCKER_IMAGE_TAG
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # Docker images for running Terraform templates on various cloud providers
 
+[![Docker Pulls](https://img.shields.io/docker/pulls/tinslice/terraform.svg?style=flat)](https://hub.docker.com/r/tinslice/terraform/)
+[![license](https://img.shields.io/github/license/tinslice/docker-terraform.svg)](https://github.com/tinslice/docker-terraform)
+
 Available images: 
 
-- terraform: `0.11.14`, `0.12.2`
-- aws-cli: `0.11.14-awscli`, `0.12.2-awscli`
+- terraform: `0.11.14`, `0.12.2`, `0.12.24`
+- aws-cli: `0.11.14-awscli`, `0.12.2-awscli`, `0.12.24-awscli`
 
 ## Usage
 
